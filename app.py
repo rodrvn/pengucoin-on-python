abaf1554611adaac20df90524343bd81e2fe0c57
rodrvn <rodrigojvallejos@gmail.com>
2023-02-26 19:41:18 -0300

    penguin-coin-full

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, jsonify, request, render_template
 from datetime import datetime
+from hashlib import sha256
+import time
+import json
 
-tiempo = datetime.today()
+tiempo = time.time()
 
 class Bloque:
-    def __init__(self, index, transacciones, timestamp):
+    def __init__(self, index, transacciones, timestamp, hash_anterior, nonce=0):
         self.index = index
         self.transacciones = transacciones
         self.timestamp = timestamp
-
+        self.hash_anterior = hash_anterior
+        self.nonce = nonce
+        
+    def hasheador(self):
+        bloque_string = json.dumps(self.__dict__, sort_keys=True)
+        
+        return sha256(bloque_string.encode()).hexdigest()
 
 class Blockchain:
     def __init__(self):
@@ -21,7 +30,8 @@ class Blockchain:
     #Para llamarlo al iniciar la blockchain.
 
     def crear_bloque_genesis(self):
-        bloque_genesis = Bloque(0, ["Genesis"], tiempo)
+        bloque_genesis = Bloque(0, ["Genesis"], tiempo, '0')
+        bloque_genesis.hash = bloque_genesis.hasheador()
         self.cadena.append(bloque_genesis)
 
 
@@ -32,11 +42,34 @@ class Blockchain:
     def ultimo_bloque(self):
         return self.cadena[-1]
     
+    
+    # Creamos la dificultad de la blockchain
+    dificultad = 2
+    def prueba_de_trabajo(self, bloque):
+        bloque.nonce = 0
+        bloque_hasheado = bloque.hasheador()
+        while not bloque_hasheado.startswith('0' * Blockchain.dificultad):
+            bloque.nonce += 1
+            bloque_hasheado = bloque.hasheador()
+        return bloque_hasheado
+    
     # Luego creamos una funcion que nos permita agregar bloques a la cadena
-    def agregar_bloque(self, bloque):
+    def agregar_bloque(self, bloque, prueba):
+        hash_anterior = self.ultimo_bloque.hash
+        if hash_anterior != bloque.hash_anterior:
+            return False
+        if not self.prueba_de_trabajo_validada(bloque, prueba):
+            return False
+        bloque.hash = prueba
         self.cadena.append(bloque)
         return True
     
+
+    # valida la prueba de trabajo
+    def prueba_de_trabajo_validada(self, bloque, hash_del_bloque):
+        return(hash_del_bloque.startswith('0' * Blockchain.dificultad) and
+               hash_del_bloque == bloque.hasheador())
+
     # Una funcion que agregue las transacciones al bloque que queremos agregar
     def agregar_transaccion(self, transaccion):
         self.transacciones_pendientes.append(transaccion)
@@ -46,12 +79,39 @@ class Blockchain:
     def cerrar_bloque(self):
         if not self.transacciones_pendientes:
             return False
+        
         ultimo_bloque = self.ultimo_bloque
-        nuevo_bloque = Bloque(ultimo_bloque.index + 1, self.transacciones_pendientes, tiempo)
-        self.agregar_bloque(nuevo_bloque)
+
+        nuevo_bloque = Bloque(ultimo_bloque.index + 1, transacciones=self.transacciones_pendientes, timestamp=tiempo, hash_anterior=ultimo_bloque.hash)
+
+        prueba = self.prueba_de_trabajo(nuevo_bloque)
+
+        self.agregar_bloque(nuevo_bloque, prueba)
+
         self.transacciones_pendientes = []
-        return nuevo_bloque
+        return nuevo_bloque.index
     
+
+# pengucoin = Blockchain()
+
+# Agregar transaccion
+# pengucoin.agregar_transaccion('Hola que tal?')
+# pengucoin.agregar_transaccion('Bien y vos')
+
+# minar
+# pengucoin.cerrar_bloque()
+
+# pengucoin.agregar_transaccion("bien tambien")
+# pengucoin.cerrar_bloque()
+
+# pengucoin.agregar_transaccion("bien tambien gracias")
+# pengucoin.cerrar_bloque()
+
+# Ver cadena
+# for bloque in pengucoin.cadena:
+#     print(bloque.__dict__)
+#     print()
+#     print('---------------------------------------------')
 
 
 app = Flask(__name__)
